3b2eb0a7b4adb1fee1723e1eed9e243ba1009c48
djo42 <dennis.john11@gmail.com>
2021-06-16 13:26:18 +0900

    Change job title display

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import React from 'react'
 // import Buttoninput from './components/buttoninput.js'
 // import Businesscard from './components/businesscard.js'
 export default function App() {
+  const hdl1 = 'Business Development | Strategy & Expansion | Digital Solutions'
+  const root = document.documentElement
+
+  console.log(hdl1.length)
+
+  root.style.setProperty('--length', hdl1.length)
+  root.style.setProperty('--lengthkey', hdl1.length + 'ch')
+
   return (
     <>
       <header></header>
       <main>
         <div className="business-card">
           <h1>Dennis John</h1>
-          <h3>
-            Business Development | Strategy & Expansion | Digital Solutions
-          </h3>
+          <h3 className="animated-text">{hdl1}</h3>
           <p>dennis@john.international</p>
         </div>
       </main>
